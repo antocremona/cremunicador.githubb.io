6ba02dd869383dab58909a590654e7fbaa3d0a28
Antonella Cremona <anto.cremona@gmail.com>
2021-09-14 00:32:13 -0300

    Incorpore AJAX en un nuevo html y con JQuery agrega pokemones que saca de la api

---
+++ scripts/ajax/poke-api.js
@@ -0,0 +1,26 @@
+
+let id = 1
+
+const urlPokeApi = 'https://pokeapi.co/api/v2/pokemon/'
+
+$('.pokeboton').click( ()=> {
+    $.get(`https://pokeapi.co/api/v2/pokemon/${id}`, (rsp) => {
+
+    id++
+
+        $('.pokemon').append(`
+        <div class="col">
+            <div class="card">
+                <img src=${rsp.sprites.front_default} class="card-img-top" alt="pokephoto">
+                <hr>
+                <div class="card-body">
+                    <h5 class="card-title">${rsp.name}</h5>
+                    <p class="card-text">${rsp.order}</p>
+                </div>
+            </div>
+        </div>
+        `)
+
+    })
+})
+
